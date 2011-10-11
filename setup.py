95655a96e7e44057603033729a0178ab1a6d3d35
Ashley Camba Garrido <ashwoods@gmail.com>
2011-10-11 19:52:12 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ VERSION = open('VERSION').read().lstrip('version: ').rstrip('\n')
 setup(name='django-hospex',
      version = VERSION,
      packages=find_packages(),
-     exclude_package_data={'rh2': ['bin/*.pyc']},
+     exclude_package_data={'hospex': ['bin/*.pyc']},
      setup_requires = ["setuptools_git >= 0.3",],
      scripts=['hospex/bin/manage.py'])
